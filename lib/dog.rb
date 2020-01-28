6ee4af2ab55efbbf37d27ed8c46d451c0c405691
vplacido <valentinplacido25@gmail.com>
2020-01-28 14:46:18 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+
+    def name
+        @name 
+    end
+
+    def name=(newName)
+        @name = newName
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(newBreed)
+        @breed = newBreed
+    end
+
+end 
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+    def name
+        @name
+    end
+
+    def job 
+        @job
+    end
+
+    def name=(newName)
+        @name = newName
+    end
+
+    def job=(newJob)
+        @job = newJob
+    end
+end
